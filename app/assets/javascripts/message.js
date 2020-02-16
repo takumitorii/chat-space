6aab8420f4d0fb67cee2ed7209bca6aa372fffe0
takumitorii <takumitorii@users.noreply.github.com>
2020-02-16 20:59:49 +0900

    Automatic_updating
    自動更新機能の実装
    
    チャットのするにあたって、リロードを不要にするため
    
    https://i.gyazo.com/544707394deecf1d155c3aafd77333d1.mp4

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function(){
   function buildHTML(message){
     if (message.image) {
       var html =
-      `<div class = "messages">
+      `<div class = "messages" data-message-id = ${message.id}>
         <div class="main-chat__contents--info">
           <div class="name">
             ${message.user_name}
@@ -22,7 +22,7 @@ $(function(){
       return html;
     } else {
         var html =
-        `<div class = "messages">
+        `<div class = "messages" data-message-id = ${message.id}>
           <div class="main-chat__contents--info">
             <div class="name">
               ${message.user_name}
@@ -64,4 +64,31 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   });
+
+  var reloadMessages = function() {
+    last_message_id = $('.messages:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log(messages)
+      if (messages.length !== 0) {
+        var insertHTML = '';
+          $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main-chat__contents').append(insertHTML);
+        $('.main-chat__contents').animate({ scrollTop: $('.main-chat__contents')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  }
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
